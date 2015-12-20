ad747e24b75392f415e76796d68d5da18b2087da
serengeor <serengeor@gmail.com>
2015-12-20 21:04:16 +0200

    Few more tests for paths and formatting.

--- src/filesystem/PathExt.cpp
+++ src/filesystem/PathExt.cpp
@@ -1,65 +1,61 @@
 #include "filesystem/PathExt.h"
 #include <algorithm>
 
-namespace PathExt
-{
-	//REFACTOR: Move to StringExt
-	std::string Replace(const std::string & source, const std::string & toReplace, const std::string & replacement)
+namespace PathExt {
+	// REFACTOR: Move to StringExt
+	std::string Replace(const std::string& source, const std::string& toReplace,
+						const std::string& replacement)
 	{
 		std::size_t index = 0;
 		std::string src = source;
 
-		while (true)
-		{
+		while (true) {
 			index = src.find(toReplace, index);
-		    if (index == std::string::npos) break;
+			if (index == std::string::npos) break;
 
-		    src.replace(index, replacement.size(), replacement);
+			src.replace(index, replacement.size(), replacement);
 			index += replacement.size();
 		}
 
 		return src;
 	}
 
-	std::string Append(const std::string & path, const std::string & pathToAppend)
+	std::string Append(const std::string& path, const std::string& pathToAppend)
 	{
 		auto size = path.size();
 		auto appendSize = pathToAppend.size();
-		
-		if(size > 0 && appendSize > 0)
-		{
-			if(path[size-1] == Separator && pathToAppend[0] == Separator)
-				return std::string(path).append(pathToAppend, 1, std::string::npos);
-			if(path[size-1] == Separator || pathToAppend[0] == Separator)
+
+		if (size > 0 && appendSize > 0) {
+			if (path[size - 1] == Separator && pathToAppend[0] == Separator)
+				return std::string(path)
+					.append(pathToAppend, 1, std::string::npos);
+			if (path[size - 1] == Separator || pathToAppend[0] == Separator)
 				return path + pathToAppend;
 			else
-				return path + Separator + pathToAppend; 
+				return path + Separator + pathToAppend;
 		}
 
 		return appendSize > 0 ? pathToAppend : path;
 	}
 
-	std::string GetFileName(const std::string & path)
+	std::string GetFileName(const std::string& path)
 	{
 		auto pos = path.find_last_of(Separator);
 
-		if(pos == std::string::npos)
-			return path;
+		if (pos == std::string::npos) return path;
 
-		return path.substr(pos+1);
+		return path.substr(pos + 1);
 	}
 
-	std::string GetExtension(const std::string & path)
+	std::string GetExtension(const std::string& path)
 	{
-		//Note: could avoid creating temp string
+		// Note: could avoid creating temp string
 		auto name = GetFileName(path);
 
-		if(name.size() > 0)
-		{
+		if (name.size() > 0) {
 			auto pos = name.find_last_of(".");
 
-			if(pos != std::string::npos && pos > 0)
-			{
+			if (pos != std::string::npos && pos > 0) {
 				return name.substr(pos);
 			}
 		}
@@ -67,34 +63,35 @@ namespace PathExt
 		return "";
 	}
 
-	std::string GetParentDirectory(const std::string & path)
+	std::string GetParentDirectory(const std::string& path)
 	{
 		auto pos = path.find_last_of(Separator);
 
-		if(pos == std::string::npos)
-			return path;
+		if (pos == std::string::npos) return path;
 
 		return path.substr(0, pos);
 	}
 
-	bool HasFileName(const std::string & path)
+	bool HasFileName(const std::string& path)
 	{
 		return GetFileName(path).size() > 0;
 	}
 
-	std::string ConvertToUnixPath(const std::string & path)
-	{ 
+	std::string ConvertToUnixPath(const std::string& path)
+	{
 		return Replace(path, WindowsSeparatorStr, SeparatorStr);
 	}
 
-	std::string Normalize(const std::string & path)
+	std::string Normalize(const std::string& path)
 	{
 		auto normalizedPath = ConvertToUnixPath(path);
 		auto duplicatePred = [](const char a, const char b) {
 			return a == Separator && b == Separator;
 		};
 
-		normalizedPath.erase(std::unique(normalizedPath.begin(), normalizedPath.end(), duplicatePred), normalizedPath.end());
+		normalizedPath.erase(std::unique(normalizedPath.begin(),
+										 normalizedPath.end(), duplicatePred),
+							 normalizedPath.end());
 
 		return normalizedPath;
 	}
