e31543297f8402179673df57ba50566cde074c92
Ernestas Januševičius <ernestasjanusevicius@gmail.com>
2016-08-25 10:19:54 -0700

    Build script updates for arguments.

--- build/build_engine.py
+++ build/build_engine.py
@@ -1,10 +1,18 @@
 try:
     from build_tools.build_tools import *
+    import argparse
 except ImportError as e:
     print("You need to fetch build_tools submodule before running this script")
 
-#example
-compiler = GetCompiler()
+#build arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-compiler", default="", required = False, choices=["gcc", "jom", "nmake", "mingw"]) #yes yes, we all know gcc is the only compiler in this list..
+parser.add_argument("-noexamples", action='store_true', help="use this to skip examples build")
+parser.add_argument("-notests", action='store_true', help="use this to skip tests")
+args = parser.parse_args()
+
+#setup
+compiler = GetCompiler(compiler = args.compiler)
 CMaker = CMakeCommand(compiler)
 
 enginePath = PathBuilder(FileSystem.Cwd()).Parent()
@@ -20,8 +28,11 @@ CMaker.Exec(buildPath,                              binPath.Join("engine"), defi
 
 #copy files
 FileSystem.CopyFiles(FileSystem.GetFilesByExtension(str(binPath), compiler.lib_extension()), libPath)
-FileSystem.CopyFolder(enginePath.Join("resources"), binPath.Join("examples/resources"))
 
 #compile tests & examples
-CMaker.Exec(enginePath.Join("test"),     binPath.Join("test"), defines = {'ENGINE_PATH':'"'+str(enginePath)+'"','gtest_disable_pthreads':'ON'})
-CMaker.Exec(enginePath.Join("examples"), binPath.Join("examples"), defines = {'ENGINE_PATH':'"'+str(enginePath)+'"'})
+if args.notests is None:
+    CMaker.Exec(enginePath.Join("test"),     binPath.Join("test"), threads = 1, defines = {'ENGINE_PATH':'"'+str(enginePath)+'"','gtest_disable_pthreads':'ON'})
+
+if args.noexamples is None:
+    FileSystem.CopyFolder(enginePath.Join("resources"), binPath.Join("examples/resources"))
+    CMaker.Exec(enginePath.Join("examples"), binPath.Join("examples"), defines = {'ENGINE_PATH':'"'+str(enginePath)+'"'})
--- build/test_runner.py
+++ build/test_runner.py
@@ -21,10 +21,13 @@ class TestRunner:
 	def Run(self):
 		self.InitTestData()
 
-		for filename in os.listdir('.'):
+		for filename in os.listdir('./'):
 			if os.path.isfile(filename):
 				st = os.stat(filename)
 				mode = st.st_mode
 				if mode & executable:
 					print("\n\n\n================ RUNNING TEST: '" + filename + "' =======================\n\n\n")
-					subprocess.check_call("./" + filename, shell=True)
+					subprocess.check_call(filename, shell=True)
+
+
+TestRunner().Run()
