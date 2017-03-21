822a37488ed380d28974de650d5b5e1831b4ff3a
Ernestas Januševičius <ernestasjanusevicius@gmail.com>
2017-03-21 12:39:05 -0700

    physfs static build, minor reformat of defines.

--- build/build_engine.py
+++ build/build_engine.py
@@ -6,7 +6,7 @@ except ImportError as e:
 
 # build arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("-compiler", default="", required = False, choices=["gcc", "jom", "nmake", "mingw"])
+parser.add_argument("-compiler", default="", required = False, choices=["gcc", "jom", "nmake", "mingw", "vs15"])
 parser.add_argument("-noexamples", action='store_true', help="use this to skip examples build")
 parser.add_argument("-notests", action='store_true', help="use this to skip tests")
 parser.add_argument("-clean", action='store_true', help="use this to delete bin directory before building")
@@ -28,10 +28,27 @@ if args.clean:
     FileSystem.RemoveDir(str(binPath))
 
 # compile libs & engine
-CMaker.Exec(enginePath.Join("third_party/physfs"),  	binPath.Join("physfs"), defines = {'PHYSFS_BUILD_TEST':'"FALSE"'})
-CMaker.Exec(enginePath.Join("third_party/glfw"),    	binPath.Join("glfw"), defines = {'GLFW_BUILD_EXAMPLES':'OFF', 'GLFW_BUILD_TESTS':'OFF', 'GLFW_BUILD_DOCS':'OFF', 'GLFW_INSTALL':'OFF'})
+CMaker.Exec(enginePath.Join("third_party/physfs"),  	binPath.Join("physfs"),
+    defines = {
+        'PHYSFS_BUILD_TEST':'"FALSE"',
+        'PHYSFS_BUILD_SHARED':'"FALSE"',
+        'PHYSFS_BUILD_STATIC':'"TRUE"'
+    })
+
+CMaker.Exec(enginePath.Join("third_party/glfw"),    	binPath.Join("glfw"),
+    defines = {
+        'GLFW_BUILD_EXAMPLES':'OFF',
+        'GLFW_BUILD_TESTS':'OFF',
+        'GLFW_BUILD_DOCS':'OFF',
+        'GLFW_INSTALL':'OFF'
+    })
+
 CMaker.Exec(enginePath.Join("third_party/glad"), 		binPath.Join("glad"))
-CMaker.Exec(buildPath,                              	binPath.Join("engine"), defines = {'ENGINE_PATH':'"'+str(enginePath)+'"'})
+
+CMaker.Exec(buildPath,                              	binPath.Join("engine"),
+    defines = {
+        'ENGINE_PATH':'"'+str(enginePath)+'"'
+    })
 
 
 # copy files
@@ -39,8 +56,15 @@ FileSystem.CopyFiles(FileSystem.GetFilesByExtension(str(binPath), compiler.lib_e
 
 # compile tests & examples
 if not args.notests:
-    CMaker.Exec(enginePath.Join("test"),     binPath.Join("test"), threads = 1, defines = {'ENGINE_PATH':'"'+str(enginePath)+'"','gtest_disable_pthreads':'ON'})
+    CMaker.Exec(enginePath.Join("test"),     binPath.Join("test"), threads = 1,
+        defines = {
+            'ENGINE_PATH':'"'+str(enginePath)+'"',
+            'gtest_disable_pthreads':'ON'
+        })
 
 if not args.noexamples:
     FileSystem.CopyFolder(enginePath.Join("resources"), binPath.Join("examples/resources"))
-    CMaker.Exec(enginePath.Join("examples"), binPath.Join("examples"), defines = {'ENGINE_PATH':'"'+str(enginePath)+'"'})
+    CMaker.Exec(enginePath.Join("examples"), binPath.Join("examples"),
+        defines = {
+            'ENGINE_PATH':'"'+str(enginePath)+'"'
+        })
