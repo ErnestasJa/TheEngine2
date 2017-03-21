6520d415932b624b395370e1a2ed7a725df676d8
Ernestas Januševičius <ernestasjanusevicius@gmail.com>
2017-03-21 10:56:43 -0700

    fix macro redefinition.

--- include/core/Debug.h
+++ include/core/Debug.h
@@ -5,19 +5,17 @@
 #define ENGINE_DEBUG
 #endif
 
-#define ASSERT(assertion)
-#define DFUNC
 
 #ifdef ENGINE_DEBUG
-
-#define ASSERT(assertion)                                                                          \
-    if ((assertion) == false) {                                                                    \
-        printf("ASSERT FAILED: %s\n", #assertion);                                                 \
-        exit(-1);                                                                                  \
-    }
-
-#define DFUNC __attribute__((noinline))
-
+	#define ASSERT(assertion)                                                                          \
+		if ((assertion) == false) {                                                                    \
+			printf("ASSERT FAILED: %s\n", #assertion);                                                 \
+			exit(-1);                                                                                  \
+		}
+	#define DFUNC __attribute__((noinline))
+#else
+	#define ASSERT(assertion)
+	#define DFUNC
 #endif
 
 #endif
